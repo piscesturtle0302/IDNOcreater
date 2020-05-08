91f1cc29f348db89ffebcc334a291e3b47d8b8fc
adanhuang0302 <adan.huang@tmnewa.com.tw>
2020-05-08 10:31:34 +0800

    first commit

---
+++ src/test/java/com/example/IDNOcreater/IdnOcreaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IDNOcreater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdnOcreaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
